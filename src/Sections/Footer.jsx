41e36d335289907283fde0554cf70c87f440ecf3
sere siaka <seresiaka@gmail.com>
2023-11-14 20:28:48 +0100

    version 5 : ajout du footer

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -1,34 +1,68 @@
 import React from 'react'
 import Sect3 from "../assets/sect3.png"
+import Search from "../assets/search1.png"
 const Footer = () => {
   return (
-    <div>
+    <div className='mt-10 text-white-400'>
     
-    <div className=' flex justify-between align-baseline  bg-cover '  >
+    <div className=' flex flex-col justify-between align-baseline  bg-cover  bg-slate-600 mt-7' >
+          <div className='flex flex-col justify-center items-center  mt-7'>
+            <div className='font-semibold text-3xl pt-5'>
+                Inscrivez-vous dans notre Newsletter 
+            </div>
+              <div className='flex '>
+             <div className='  rounded-lg p-5 '>
+                <form>
+                    <input className=' px-20 w-95 h-20' type="text" placeholder='' />
+                </form>
+             </div>
+             <div className=' ' >
+                <button  className='my-5  '> 
+                <img src={Search} className=" rounded-full " width={83} height={60}/>
+                </button>
+             </div>
+             </div>
+          </div>
+          <div  className='my-5' >
+              <div>Plus besoin de dire adieu
+                       aux 
+                objets que vous aimez.
+                </div>
+                <div>Revendeurs
+                      Particuliers
+                Carrières ↗
+               FAQ
+                </div>
+               <div>
+               Cookies
+                Confidentialité
+                Mentions légales
+                CGU
+               </div>
+          </div>
+          <div>
+             
+          Evy est une Société par Actions Simplifiée (SAS) au capital de 1 955.08 €, 
+          inscrite au registre du commerce et des sociétés 
+          de Paris sous le numéro 902 019 819, et dont le siège social est situé 38 
+          rue des Mathurins à Paris (75008).
+          </div>
+          <div>
+              <div>
+              © Copyright 2023 Evy. Tous droits réservés.
+              </div>
+              <div>
+                <a target="_blank" href='linkedin.com/in/siaka-sere/'> LinkedIn ↗</a>
+              </div>
+             
+          </div>
 
 
-<div className=' my-10 mx-10 align-middle w-3/6  '> 
-<div className='mt-15 pt-9'>
-<div className='mt-20 pt-10 font-sans font-bold text-6xl my-10 pb-7 align-baseline  bg-white'>
-Contre les vrais imprévus
-</div>
-<p className='text-1xl  mt-10 pt-10 font-sans'>
-
-Sholip est là pour les petits incidents du quotidien parce qu’ils font partie de la vie. Et surtout, on vous dit clairement contre quoi vos objets sont protégés. Totale clarté.
-
-<span className='flex flex-col mt-3'>C’est la vie</span>
-  </p>
   
-</div>
- </div>
- <div className='mt-10   p-3'> 
 
-<img  src={Sect3} className="  "  width={800} height={60}/>
 
- </div>
-</div>  
- <br/>
 
+    </div>
     </div>
   )
 }
